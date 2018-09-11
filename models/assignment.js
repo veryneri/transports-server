ac8c0e051b09b8e460a4fe931eaeeb94cdccb55e
Edson Neri <edson.neri@correo.uia.mx>
2018-09-11 15:03:56 -0500

    adds assignment validations
      - Employee exists
      - Vehicle exists
      - Neither Employee nor Vehicle is already in an active assignment

--- models/assignment.js
+++ models/assignment.js
@@ -29,4 +29,95 @@ var AssignmentSchema = new Schema({
   },
 });
 
+AssignmentSchema.path('employee').validate(
+  function(value) {
+    return new Promise(function(resolve, reject) {
+      mongoose.models['Employee'].findOne(
+        {_id: value},
+        function(err, employee) {
+          if (err) reject(err);
+          var employeeExists = false;
+          if (employee)
+            employeeExists = true;
+
+          resolve(employeeExists);
+        }
+      );
+    });
+  },
+  'Employee does not exists'
+);
+
+AssignmentSchema.path('vehicle').validate(
+  function(value) {
+    return new Promise(function(resolve, reject) {
+      mongoose.models['Vehicle'].findOne(
+        {_id: value},
+        function(err, vehicle) {
+          if (err) reject(err);
+          var vehicleExists = false;
+          if (vehicle)
+            vehicleExists = true;
+
+          resolve(vehicleExists);
+        }
+      );
+    });
+  },
+  'Vehicle does not exists'
+);
+
+AssignmentSchema.pre('save', function(next) {
+  var newAssignment = this;
+  var customAPIError = require('../errors').customAPIError;
+
+
+  mongoose.models['Assignment'].findOne(
+    {
+      $or: [
+        {
+          employee: newAssignment.employee,
+          active: true,
+        },
+        {
+          vehicle: newAssignment.vehicle,
+          active: true,
+        },
+      ],
+    },
+    function(err, currentAssignment) {
+      console.log(currentAssignment);
+      if (err)
+        return next(err);
+
+      if (currentAssignment) {
+        if (
+          currentAssignment.vehicle.toString() ===
+          newAssignment.vehicle.toString()
+        ) {
+          err = customAPIError(
+            'VehicleAlreadyAssignedError',
+            'Vehicle is already assigned.',
+            400
+          );
+        }
+        if (
+          currentAssignment.employee.toString() ===
+          newAssignment.employee.toString()
+        ) {
+          err = customAPIError(
+            'EmployeeAlreadyAssignedError',
+            'Employee is already assigned.',
+            400
+          );
+        }
+
+        return next(err);
+      }
+
+      return next();
+    }
+  );
+});
+
 module.exports = mongoose.model('Assignment', AssignmentSchema);
